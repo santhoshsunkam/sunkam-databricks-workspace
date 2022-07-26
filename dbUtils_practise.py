9c6176ba12ff39b6b4d6f9fe271c453f3198e884
mrsunkam@gmail.com <mrsunkam@gmail.com>
2022-07-26 03:33:35 +0000

    july27 commits on dbutils fs
    dbutils fs practise

--- dbUtils_practise.py
+++ dbUtils_practise.py
@@ -9,4 +9,32 @@ dbutils.fs.help()
 
 # COMMAND ----------
 
+# Databricks root directory 
+dbutils.fs.ls("dbfs:/")
+
+# COMMAND ----------
+
+# databricks provides sample files in this directory 
+dbutils.fs.ls("dbfs:/databricks-datasets/")
+
+# COMMAND ----------
+
+# dbfs:/FileStore/
+dbutils.fs.ls("dbfs:/FileStore/")
+
+# COMMAND ----------
+
+dbutils.fs.ls("dbfs:/databricks-datasets/")
+
+# COMMAND ----------
+
+dbutils.fs.help()
+
+# COMMAND ----------
+
+# Copying from one loc to other loc here I am c
+dbutils.fs.cp("databricks-datasets/amazon/README.md","dbfs:/FileStore/")
+
+# COMMAND ----------
+
 
