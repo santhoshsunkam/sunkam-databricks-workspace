e35a0f4aa6ae107d74b835ec8c25e59610628f68
mrsunkam@gmail.com <mrsunkam@gmail.com>
2022-07-30 17:05:53 +0000

    dbutils -demo - july 30

--- PythonForDatabricks/dbutils_demo.py
+++ PythonForDatabricks/dbutils_demo.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+#dbutils help
 dbutils.help()
 
 # COMMAND ----------
@@ -7,11 +8,53 @@ dbutils.widgets.help()
 
 # COMMAND ----------
 
+#dbutils text widget widget name is x 
 dbutils.widgets.text("x","","enter x value")
 
 # COMMAND ----------
 
+#dbutils text widget value return from textbox above 
 dbutils.widgets.get("x")
+
+# COMMAND ----------
+
+# create a dict with country name and capital 
+country_name = {"India":"New Delhi", "Japan":"Toyko", "Canada":"Ottava","United Kingdom": "London"}
+
+# COMMAND ----------
+
+#print dictionary 
+print(country_name)
+
+# COMMAND ----------
+
+# display dict values using for loop 
+for k, v in country_name.items():
+    print(" value {} and {}".format(k,v))
+    
+
+# COMMAND ----------
+
+# function to return country capita from dictionary 
+def countrydetails(country):
+    capital =country_name.get(country,"Invalid country")
+    return capital
+
+# COMMAND ----------
+
+# text widget using function 
+def countryWidget(country):
+    dbutils.widgets.text("country","","Enter country name")
+    x=dbutils.widgets.get("country")
+    return x
+
+# COMMAND ----------
+
+# return country values from dictionary by taking country inout from widget 
+x = countryWidget(x)
+print("Input Country:"+x)
+cap =countrydetails(x)
+print("capital :"+cap)
 
 # COMMAND ----------
 
