a76b12b64e79c194dce0328e1eba0d658b8d8e88
mrsunkam@gmail.com <mrsunkam@gmail.com>
2022-07-30 16:35:28 +0000

    dbutils demo - july 30

---
+++ PythonForDatabricks/dbutils_demo.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+dbutils.help()
+
+# COMMAND ----------
+
+dbutils.widgets.help()
+
+# COMMAND ----------
+
+dbutils.widgets.text("x","","enter x value")
+
+# COMMAND ----------
+
+dbutils.widgets.get("x")
+
+# COMMAND ----------
+
+
