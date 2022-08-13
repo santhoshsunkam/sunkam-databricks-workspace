1e21693e9314958d701f2e34b94d7ecb0a95585e
mrsunkam@gmail.com <mrsunkam@gmail.com>
2022-08-13 08:38:28 +0000

    read_file_using_abfss

---
+++ All-Topics-Demos/FileRead_using_ABFSS_driver.py
@@ -0,0 +1,49 @@
+# Databricks notebook source
+# sunkamsecrets  
+# to create secretes scrope - https://adb-<instance>.10.azuredatabricks.net/#secrets/createScopes
+dbutils.secrets.listScopes()
+
+# COMMAND ----------
+
+dbutils.secrets.list("sunkamScope")
+
+# COMMAND ----------
+
+spark.conf.set(
+    "fs.azure.account.key.blobsourcestg.dfs.core.windows.net",
+    dbutils.secrets.get(scope="sunkamScope", key="blobsource"))
+
+# COMMAND ----------
+
+src_file_path="abfss://input@blobsourcestg.dfs.core.windows.net/*.csv"
+
+# COMMAND ----------
+
+df2 = spark.read.format("csv").option("header",True).load(src_file_path)
+
+# COMMAND ----------
+
+
+df.printSchema()
+
+# COMMAND ----------
+
+display(df2)
+
+# COMMAND ----------
+
+spark.conf.set(
+    "fs.azure.account.key.blobsinkstg.dfs.core.windows.net",
+    dbutils.secrets.get(scope="sunkamScope", key="blobsink"))
+
+# COMMAND ----------
+
+target_file_path = "abfss://output@blobsinkstg.dfs.core.windows.net"
+
+# COMMAND ----------
+
+df2.write.option("header",True).csv("abfss://output@blobsinkstg.dfs.core.windows.net/EMP/")
+
+# COMMAND ----------
+
+
